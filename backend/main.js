3d8a707ac11d58c657831fc1847a649101152703
Mitsukukki <55334852+Mitsukukki@users.noreply.github.com>
2020-06-21 21:06:36 +0200

    The frontend is now connected to the backend

--- backend/main.js
+++ backend/main.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const {resolve} = require("path");
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
 	res.send('Hello World!');
-});
+});*/
+
+app.use(express.static('../frontend'));
 
 app.listen(42069, function () {
 	console.log('Example app listening on port 3000!');
-	nw.Window.open(resolve("../frontend/index.html"), {}, function(win) {});
+	nw.Window.open("http://127.0.0.1:42069/index.html", {}, function(win) {
+		win.setPosition(center);
+		win.resizeTo(250,250)
+		//win.show(true);
+	});
 });
