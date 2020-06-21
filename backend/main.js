d3dc42eb50c406dbb47a8ec812913fbfc80b040c
Mitsukukki <55334852+Mitsukukki@users.noreply.github.com>
2020-06-21 18:28:04 +0200

    Open the main window of the app.

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const {resolve} = require("path");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
 
 app.listen(42069, function () {
-  console.log('Example app listening on port 3000!')
-})
+	console.log('Example app listening on port 3000!');
+	nw.Window.open(resolve("../frontend/index.html"), {}, function(win) {});
+});
